427979a9edaae7618d9301fc625835515480c72a
Jennifer <jencegram@users.noreply.github.com>
2024-04-13 01:55:58 -0400

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,41 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 require('dotenv').config();
+
+
 const app = express();
 const userRoutes = require('./routes/users');
 const bookRoutes = require('./routes/books');
 const bookClubRoutes = require('./routes/bookClubs');
-const stateRoutes = require('./routes/states'); 
+const stateRoutes = require('./routes/states');
 const profileRoutes = require('./routes/profile');
 const genreRoutes = require('./routes/genreRoutes');
 const postsRouter = require('./routes/posts');
 const forumsRouter = require('./routes/forums');
 
-app.use((req, res, next) => {
-  next();
-});
+// Serve static files from frontend build directory
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
 
 app.use(cors());
 app.use(express.json());
+
+
 app.use('/api', userRoutes);
 app.use('/api', bookRoutes);
 app.use('/api', bookClubRoutes);
-app.use('/api/states', stateRoutes); 
+app.use('/api/states', stateRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/genres', genreRoutes)
 app.use('/api/posts', postsRouter);
 app.use('/api/forums', forumsRouter);
 app.use('/api/users', userRoutes);
+
+// Handle React routing, return all requests to React app
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+});
 
 // Conditional listen
 if (require.main === module) {
